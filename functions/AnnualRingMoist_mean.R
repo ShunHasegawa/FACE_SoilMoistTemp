9f546f1c77ffa6c4ab0eab9d588acd970cf4e08d
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-03-24 14:35:30 +1100

    plyr is used

--- functions/AnnualRingMoist_mean.R
+++ functions/AnnualRingMoist_mean.R
@@ -1,26 +1,25 @@
-#annual ring mean moisture
+# annual ring mean moisture
 
-#extract soil moisture data
-soil.mo.mean<-allsoil[,grep("VWC|ring|Date$|co2",names(allsoil))]
-head(soil.mo.mean)
-summary(soil.mo.mean)
+# extract soil moisture data
+soil.mo <- allsoil[,grep("VWC|ring|Date$|co2",names(allsoil))]
 
 #annual ring mean
-vals<-names(soil.mo.mean)[grep("VWC",names(soil.mo.mean))]
-an.ring<-with(soil.mo.mean,aggregate(soil.mo.mean[vals],
-                                     list(ring=ring),mean,na.rm=TRUE))
-summary(an.ring)
+an.colmeans <- function(x){
+  colMeans(x[, 1:8], na.rm = TRUE)
+}
 
-#annual mean & SE
-#meltdataset
-an.ml<-melt(an.ring,id="ring")
-summary(an.ml)
-an.mean<-summaryBy(value~ring,FUN=c(mean,function(x) ci(x)[4],length),
-                   fun.names=c("mean","SE","Sample_size"),data=an.ml)
-an.mean
-names(an.mean)[1:3]<-c("ring","mean","se")
-#change unit from fraction to %
-an.mean$mean<-an.mean$mean*100
-an.mean$se<-an.mean$se*100
+an.ring.mean <- ddply(soil.mo, .(ring, co2), 
+                      function(x) colMeans(x[, 1:8]*100, na.rm = TRUE))
+
+# function which produces mean and SE
+an.mean.se <- function(x){
+  data.frame(Mean = mean(x, na.rm = TRUE), 
+             SE = ci(x, na.rm = TRUE)[4],
+             N = sum(!is.na(x)))
+}
+
+an.mean.se <- apply(an.ring.mean[, 3:10], 1, an.mean.se)
+an.mean <- cbind(ring = c(1:6), rbind.fill(an.mean.se))
+
 #save
 save(an.mean,file="output/an.mean.Rdata")
