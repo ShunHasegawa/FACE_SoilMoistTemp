f5605f7adccf11f4286f7520f7164cf26f4325bb
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-03-24 19:42:54 +1100

    plot with ggplot

---
+++ functions/fig.soil.moist.5cm.N8.R
@@ -0,0 +1,12 @@
+theme_set(theme_bw())
+p <- ggplot(ring.means, aes(x = Date, y = moist * 100, col = ring, linetype = ring))
+pl <- p + geom_line(size = 1) + 
+  scale_color_manual(values = palette(), "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
+  scale_linetype_manual(values = c("solid", "dashed", "dashed", "solid", "solid", "dashed"), 
+                        "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
+  labs(x = "Time", y = "Soil moisture (% of volumetrtic water content)") +
+  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed") +
+  ggtitle("n = 8")
+
+ggsave(filename = "Figs/FACE.Soil.Moist.at.5cm.png", plot = pl)
+ggsave(filename = "Figs/FACE.Soil.Moist.at.5cm.pdf", plot = pl)
