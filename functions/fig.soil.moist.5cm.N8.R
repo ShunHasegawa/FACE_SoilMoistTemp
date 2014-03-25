bd2fc6225ac78ef1fa0be72b9d2c3031f9b50e19
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-03-25 22:11:00 +1100

    main title is removed

--- functions/fig.soil.moist.5cm.N8.R
+++ functions/fig.soil.moist.5cm.N8.R
@@ -4,9 +4,9 @@ pl <- p + geom_line(size = 1) +
   scale_color_manual(values = palette(), "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
   scale_linetype_manual(values = c("solid", "dashed", "dashed", "solid", "solid", "dashed"), 
                         "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
-  labs(x = "Time", y = "Soil moisture (% of volumetrtic water content)") +
-  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed") +
-  ggtitle("n = 8")
+  labs(x = "Time", y = "Soil moisture (% of volumetrtic water content) (n+8)") +
+  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed")
+  
 
 ggsave(filename = "Figs/FACE.Soil.Moist.at.5cm.png", width = 8, height = 4, units= "in", plot = pl)
 ggsave(filename = "Figs/FACE.Soil.Moist.at.5cm.pdf", width = 8, height = 4, units= "in", plot = pl)
