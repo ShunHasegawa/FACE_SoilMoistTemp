d413f41148020d5079219cc44bdc7225f1699476
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-03-25 11:01:43 +1100

    graph modified

--- functions/Fig.soil.moist.dif.depths.R
+++ functions/Fig.soil.moist.dif.depths.R
@@ -10,10 +10,10 @@ pl <- p + geom_line(size = 1) +
   scale_color_manual(values = palette(), "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
   scale_linetype_manual(values = c("solid", "dashed", "dashed", "solid", "solid", "dashed"), 
                         "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
-  labs(x = "Time", y = "Soil moisture (% of volumetrtic water content)") +
+  labs(x = "Time", y = "Soil moisture (% of volumetrtic water content) (n=2)") +
   facet_grid(variable ~. ) +
-  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed") +
-  ggtitle("n = 2")
+  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed")
+  
 
 ggsave(filename = "Figs/FACE.Soil.Moist.diff.layers.png", plot = pl)
 ggsave(filename = "Figs/FACE.Soil.Moist.diff.layers.pdf", plot = pl)
