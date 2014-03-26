fd997bae0976850c9738cdb863a2f478b46ec550
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-03-26 20:28:42 +1100

    change line type

--- functions/Fig.soil.moist.dif.depths.R
+++ functions/Fig.soil.moist.dif.depths.R
@@ -8,11 +8,11 @@ theme_set(theme_bw())
 p <- ggplot(moist.depths.mlt, aes(x = Date, y = value, col = ring, linetype = ring))
 pl <- p + geom_line(size = 1) + 
   scale_color_manual(values = palette(), "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
-  scale_linetype_manual(values = c("solid", "dashed", "dashed", "solid", "solid", "dashed"), 
+  scale_linetype_manual(values = c("solid", "dotted", "dotted", "solid", "solid", "dotted"), 
                         "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
   labs(x = "Time", y = "Soil moisture (% of volumetrtic water content) (n=2)") +
   facet_grid(variable ~. ) +
-  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed")
+  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dotted")
   
 
 ggsave(filename = "Figs/FACE.Soil.Moist.diff.layers.png", plot = pl)
