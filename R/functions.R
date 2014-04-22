4320b19d3cc56f05853dce408c53100c22a560d3
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-22 21:15:13 +1000

    added to version control

---
+++ R/functions.R
@@ -0,0 +1,14 @@
+##################################################################
+# function which processes dataset (add ring #, co2 treatments)  #
+##################################################################
+processData <- function(dataset) {
+  #remvoe duplicate
+  dataset <- dataset[!duplicated(dataset),]
+  #add ring
+  dataset$ring <- factor(substr(dataset$Source,7,7))
+  #add co2
+  dataset$co2 <- factor(ifelse(dataset$ring %in% c(1,4,5), "elev", "amb"))
+  #add date
+  dataset$Date <- as.Date(dataset$DateTime)
+  return(dataset)
+} 
