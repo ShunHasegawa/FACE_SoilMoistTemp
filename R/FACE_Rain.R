d8ece58dfd6bc170bd57cf8b1d5a607647d44d7b
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-22 22:32:58 +1000

    old scripts were removed

--- R/FACE_Rain.R
+++ R/FACE_Rain.R
@@ -10,15 +10,6 @@
 
 setToken("wSoiD4s3UDMzxwdNhzmG ")
 
-##download up to date files from HIEv
-##files <- searchHIEv(filename="FACELawn_FACE_Rain")
-##files to be newly downloaded
-##newfiles <- files$filename[files$file_size > file.info(files$filename)["size"]|
-##                             !file.exists(files$filename)]
-##files$file_size: file size on HIEv
-##file.info(files$filename[i])["size"]: file size in the folder (already downloaded before)
-##file.exists: if files on HIEv have been downloaded
-
 #download, process and combine
 rains <- downloadTOA5("FACELawn_FACE_Rain", cachefile = "Data/hievdata/FACE_Rain_tmp.RData", 
                       topath = "Data/hievdata/row_data")
