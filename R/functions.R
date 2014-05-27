f883b38cfde50f9599a6b19abb986adc1baebf8a
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-27 12:22:03 +1000

    subset and ggplot saving funcitons are added

--- R/functions.R
+++ R/functions.R
@@ -49,3 +49,26 @@ pltAllvar <- function(data, filetitle){
   dev.off()
 }
 
+#########################
+# subset and droplevels #
+#########################
+subsetD <- function(...){
+  droplevels(subset(...))
+}
+
+##############################
+# Save ggplot in PDF and PNG #
+##############################
+ggsavePP <- function(filename, plot, width, height){
+  ggsave(filename = paste(filename, ".pdf", sep = ""), 
+         plot = plot, 
+         width = width, 
+         height = height)
+  
+  ggsave(filename = paste(filename, ".png", sep = ""), 
+         plot = plot, 
+         width = width, 
+         height = height, 
+         dpi = 600)
+}
+
