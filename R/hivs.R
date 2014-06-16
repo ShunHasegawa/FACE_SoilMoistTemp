acf45e4b262eb0f4e2ea96cc04e2bfa06c2e963c
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-06-16 13:57:44 +1000

    hiev library is updated

--- R/hivs.R
+++ R/hivs.R
@@ -4,7 +4,7 @@ install.packages("devtools")
 #load the devtools package, so that it can be used. 
 library(devtools)
 #install the HIEv package, and then load it so that it can be used
-install_bitbucket("HIEv","remkoduursma",ref="dev")
+install_bitbucket("HIEv","remkoduursma")
 
 ?install_bitbucket
 
