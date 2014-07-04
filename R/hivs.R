4266199c6992da9de1c3ac5cb38643f50c14e117
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-04 18:10:14 +1000

    ...

--- R/hivs.R
+++ R/hivs.R
@@ -1,7 +1,6 @@
-#install the devtools package, which is required to install the HIEv package if necessary
-#####
+# install the devtools package, which is required to install
+# the HIEv package if necessary
 install.packages("devtools")
-#load the devtools package, so that it can be used. 
 
 #install the HIEv package, and then load it so that it can be used
 devtools::install_bitbucket("HIEv","remkoduursma")
