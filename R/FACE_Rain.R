166c957f4b65cb3c94a5b928acfdd0b19b2a7592
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-08 21:10:58 +1000

    codes are repolished

--- R/FACE_Rain.R
+++ R/FACE_Rain.R
@@ -1,24 +1,9 @@
-#rm(list=ls(all=TRUE))
-#library to download files from HIVe
-#library(devtools)
-#library(HIEv)
+# set token if you haven't done so yet
+# setToken(tokenfile = "Data//token.txt")
 
-#library
-#source("functions/list_library.R")
-#(.packages()) 
-
-
-setToken("wSoiD4s3UDMzxwdNhzmG ")
-
-#download, process and combine
+# download, process and combine
 rains <- downloadTOA5("FACELawn_FACE_Rain", cachefile = "Data/hievdata/FACE_Rain_tmp.RData", 
                       topath = "Data/hievdata/row_data")
-
-
-#summary(rains)
-
-#reorder accoding to DateTime
-rains <- rains[order(rains$DateTime),]
 
 # remove duplicates
 rains <- rains[!duplicated(rains),]
@@ -26,45 +11,39 @@ rains <- rains[!duplicated(rains),]
 # save
 save(rains,file="output//Data/rains.RData")
 
-#write.csv(rainsn,"output/FACE_Rain(15min).csv")
-
-#daily accumulative rainfall
+# daily accumulative rainfall
 DailyRain <- ddply(rains, .(Date), function(x) colSums(x[c("Rain_mm_Tot", "Through_Tot")], na.rm = TRUE))
 
-#save
-save(DailyRain,file="output/Data/DailyRain.RData")
+# save
+save(DailyRain, file="output/Data/DailyRain.RData")
 
-#write.csv(rain.d,"output/FACE_Rain(daily).csv")
-#plot(Rain_mm_Tot~Date,type="h",data=DailyRain)
+##############################################################
+# Combine with rainfall data measureed before september 2012 #
+##############################################################
 
-#combine with previous datasets
-#call it
+# Call data
 load("output/Data/Rainbefore.Rdata")
-#summary(Rainbefore)
 
-#check if rainfall data match between HIEv and Vinol's file
-  #remove 0
-#  nr <- subset(Rainbefore,Rainbefore$Rain_mm_Tot!=0)
-#  plot(Rain_mm_Tot~Date,data=nr,type="p")
-  
-#  dr <- subset(DailyRain,DailyRain$Rain_mm_Tot!=0)
-#  points(Rain_mm_Tot~Date,data=dr,type="p",col="red")
-#  dr$Rain_mm_Tot[dr$Date==as.Date("2013-06-03")]
-#  #2013-06-03
-#  #HIEv: rainfall = 0
-#  #Vinol's file: rainfall = 20.5
-#  #I use vinol's file this time for the data on this data.
-#  #delete 2013-06-03 measurement from DailyRain
-#  DailyRain <- DailyRain[-which(DailyRain$Date=="2013-06-03"),]
+# check if rainfall data match between HIEv and Vinol's file
 
-#combine
-#summary(DailyRain)
-  #Measurements have begun on 2013-06-03
+# remove 0
+RB <- subset(Rainbefore,Rainbefore$Rain_mm_Tot!=0)
+Rhiv <- subset(DailyRain,DailyRain$Rain_mm_Tot!=0)
 
-#extract data between 2012-09-20 to 2013-06-03 from Vinol's data (Rainbefore)
-before <- subset(Rainbefore,Date>="2012-09-20"&Date<"2013-06-04")
-#summary(before)
+# plot
+plot(Rain_mm_Tot ~ Date,data=RB, type="p")
+points(Rain_mm_Tot~Date, data=Rhiv, type="p", col="red")
+  # They are pretty much the same, so just use Rainbefore for not overwrapped
+  # periods (i.e. before 2012-09-03)
 
-allrain <- rbind.fill(before,DailyRain) #the number of columes is different so use rbind.fill rather than rbind
+# rain data between 2012-06-01 and 2012-9-3
+rbDF <- subset(Rainbefore, Date < as.Date("2012-09-03") & Date > as.Date("2012-06-01"))
 
+# Combind
+allrain <- rbind.fill(rbDF, DailyRain) 
+# the number of columes is different so use rbind.fill rather than rbind
+
+plot(Rain_mm_Tot ~ Date, data = allrain, type = "h")
+
+# save
 save(allrain,file="output/Data/allrain.RData")
