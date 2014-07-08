2d787af07fe19bec0c64e3d061cd74136cf13f26
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-09 09:12:55 +1000

    axis and ggsavePP are updated

--- R/fig.soil.moist.5cm.N8.R
+++ R/fig.soil.moist.5cm.N8.R
@@ -4,9 +4,9 @@ pl <- p + geom_line(size = 1) +
   scale_color_manual(values = palette(), "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
   scale_linetype_manual(values = c("solid", "longdash", "longdash", "solid", "solid", "longdash"), 
                         "Ring", labels = paste("Ring", c(1:6),sep = "_")) +
+  scale_x_date(breaks= date_breaks("2 month"), labels = date_format("%b-%y")) +
+  theme(axis.text.x  = element_text(angle=45, vjust= 1, hjust = 1)) +
   labs(x = "Time", y = "Soil moisture (% of volumetrtic water content)(n=8)") +
   geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "longdash")
   
-
-ggsave(filename = "output/Figs/FACE.Soil.Moist.at.5cm.png", width = 8, height = 5, units= "in", plot = pl)
-ggsave(filename = "output/Figs/FACE.Soil.Moist.at.5cm.pdf", width = 8, height = 5, units= "in", plot = pl)
+ggsavePP(filename = "output/Figs/FACE.Soil.Moist.at.5cm", width = 8, height = 5, plot = pl)
