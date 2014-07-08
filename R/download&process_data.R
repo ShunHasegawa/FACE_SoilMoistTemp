1c2e8a57ab29451978f8b4cc0903c7290135e945
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-08 18:25:38 +1000

    line mergin is readjested

--- R/download&process_data.R
+++ R/download&process_data.R
@@ -1,13 +1,18 @@
+##############################################
+# Install/update HIE if you haven't done yet #
+##############################################
+# source("R/hivs.R")
+
 #######################################
 # download up to date files from HIEv #
 #######################################
 setToken(tokenfile = "Data/token.txt")
 
 # download files from HIEv
-# use chachefile function in download TOA5
+# use chachefile function in downloadTOA5
 allsoils <- downloadTOA5("SoilVars",cachefile="Data/hievdata/tmp.RData",
                       topath="Data/hievdata/row_data", maxnfiles = 100)
-#old files are stored in c:/hievdata/tmp.RData
+# previous files are stored in hievdata/tmp.RData
 
 # remove duplicates and add ring and co2 columns
 allsoil <- processData(allsoils)
@@ -17,9 +22,8 @@ allsoil <- processData(allsoils)
 # Cleaning data #
 #################
 
-# There are some weird values so remove so need to clean up 
-# data. NOTE: need to check again I just do it quickly this
-# time.
+# There are some weird values so remove so need to clean up data. NOTE: need to
+# check again I just do it quickly this time.
 
 #############################################
 ## visually inspect by plottint all probes ##
@@ -29,21 +33,19 @@ source("R/PlotAllProbes.R")
 # there is a REALLY weird value in VWC_1_Avg
 summary(allsoil$VWC_1_Avg)
 summary(allsoil$VWC_2_Avg)
-# VWC is soil moisture so can't be larger than 1 (or evern
-# 0.4)
+
+# VWC is soil moisture so can't be larger than 1 (or evern 0.4)
 allsoil <- subset(allsoil,VWC_1_Avg < 1 & VWC_2_Avg <1)
 
-# Most of the probes are not working properly in the 1-2nd 
-# weeks so remove all data till till ##August## for the time
-# being
+# Most of the probes are not working properly in the 1-2nd weeks so remove all
+# data till till ##August## for the time being
 allsoil <- subset(allsoil, Date >= as.Date("2012-08-01"))
 
 # ring2_Theta30_2_Avg is not working at all..
 allsoil$Theta30_2_Avg[allsoil$ring == 2] <- NA
 
 save(allsoil, file="output/Data/allsoil.RData")
-# Note: save as binary (which is quicker to process than
-# csv)
+# Note: save as binary (which is quicker to process than csv)
 
 ##########################################
 # Daily min, max and mean for each probe #
