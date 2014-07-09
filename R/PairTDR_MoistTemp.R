b505307fadee3383780a1f63f45e8ea761610633
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-09 10:23:26 +1000

    plot moisture for eachring against the ohters

--- R/PairTDR_MoistTemp.R
+++ R/PairTDR_MoistTemp.R
@@ -1,4 +1,4 @@
-load("output//Data//FACE_SoilAllProb.RData")
+# load("output//Data//FACE_SoilAllProb.RData")
 
 # subet TDR
 TDR <- subsetD(soilRngSmry, grepl("TDR|VWC", variable))
@@ -26,6 +26,8 @@ names(PairTDR_MoistTemp)[grep("Moist", names(PairTDR_MoistTemp))] <- "Moist"
 #######
 # Fig #
 #######
+
+## Moist vs Temp
 theme_set(theme_bw())
 
 temps <- names(PairTDR_MoistTemp)[grep("Temp", names(PairTDR_MoistTemp))]
@@ -42,3 +44,13 @@ l_ply(temps, function(x)
   ggsavePP(filename = paste("output//Figs/FACE_Moist_", x, sep = ""),
            plot = PltMoistTemp(data = PairTDR_MoistTemp, x , size = .5), 
            width = 6, height = 4))
+
+## Plot moisture against each plot
+RngMoistMean <- ddply(PairTDR_MoistTemp, .(Date, ring), summarise, value = mean(Moist, na.rm = TRUE)) 
+RngMoist <- cast(RngMoistMean, Date ~ ring)
+names(RngMoist)[-1] <- paste("Ring_", names(RngMoist)[-1], sep = "")
+scatterplotMatrix(~ Ring_1 + Ring_2 + Ring_3 + Ring_4 + Ring_5 + Ring_6, 
+                  data = RngMoist, 
+                  diag = "boxplot",
+                  lwd = 2,
+                  smoother = FALSE)
