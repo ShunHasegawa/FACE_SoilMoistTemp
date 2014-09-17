a1d26934608efbca33d758568f5111914b1a738d
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-09-17 14:20:13 +0100

    an.mean.SE: object names was the same as function, so it's modified

--- R/AnnualRingMoist_mean.R
+++ R/AnnualRingMoist_mean.R
@@ -6,8 +6,8 @@ soil.mo <- allsoil[,grep("VWC|ring|Date$|co2",names(allsoil))]
 an.ring.mean <- ddply(soil.mo, .(ring, co2), 
                       function(x) colMeans(x[, 1:8]*100, na.rm = TRUE))
 
-an.mean.se <- apply(an.ring.mean[, 3:10], 1, an.mean.se)
-an.mean <- cbind(ring = factor(c(1:6)), rbind.fill(an.mean.se))
+an.mean.SE <- apply(an.ring.mean[, 3:10], 1, an.mean.se)
+an.mean <- cbind(ring = factor(c(1:6)), rbind.fill(an.mean.SE))
 
 #save
 save(an.mean,file="output/Data/an.mean.RData")
