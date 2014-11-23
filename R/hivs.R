d9fc6562d39c8f355ce1cf0290a00dfe393c8a45
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-11-23 03:17:52 +0000

    path has changed

--- R/hivs.R
+++ R/hivs.R
@@ -3,5 +3,5 @@
 install.packages("devtools")
 
 #install the HIEv package, and then load it so that it can be used
-devtools::install_bitbucket("HIEv","remkoduursma")
+devtools::install_bitbucket("remkoduursma/HIEv")
 
