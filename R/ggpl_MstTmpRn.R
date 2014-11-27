f01c2f96579c30944853ce39ac8ed4ba4878ea03
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-11-27 18:22:09 +0000

    figs for poster

--- R/ggpl_MstTmpRn.R
+++ R/ggpl_MstTmpRn.R
@@ -7,11 +7,11 @@ FgMstTmpRn <- function(startDate = NULL, endDate = NULL){
   #############
   # temp mean #
   #############
-  tempDF <- subsetD(soilRngSmry, grepl("TDRTemp", variable))
+  tempDF <- subsetD(data.frame(soilRngSmry), grepl("TDRTemp", variable))
   DayTemp <- ddply(tempDF, .(Date), summarise, Mean = mean(Mean, na.rm = TRUE), 
                    Min = mean(Min, na.rm = TRUE), 
                    Max = mean(Max, na.rm = TRUE))
-  DayTemp$variable <- "atop(Soil~temperature, at~10~cm~(degree~C))"
+  DayTemp$variable <- "atop(Soil~temperature, at~15~cm~(degree~C))"
   
   
   ############
@@ -26,16 +26,34 @@ FgMstTmpRn <- function(startDate = NULL, endDate = NULL){
   # load("output/Data/co.means.RData")
   # change unit
   co.means$moist <- co.means$moist * 100
-  co.means$variable <- "Soil~moisture*~('%')"
+  co.means$variable <- "atop(Soil~moisture, at~15~cm*~('%'))"
   
+  ####################
+  # Blank data frame #
+  ####################
+  variables <- sapply(list(DayTemp, co.means, allrain), function(x) unique(x$variable))
+  
+  # This is gaoing to be the base of the 3 figs
+  blankDF <- data.frame(Date = as.Date("2013-01-01"), 
+                        Mean = c(median(DayTemp$Mean), 
+                                 median(co.means$moist),
+                                 median(allrain$Rain_mm_Tot)),
+                        variable = variables
+                        )
+  blankDF$variable <- factor(blankDF$variable, levels = variables)
   ########
   # plot #
   ########
   theme_set(theme_bw())
-  p <- ggplot(DayTemp, aes(x =  Date, y = Mean))
+  p <- ggplot(blankDF, aes(x =  Date, y = Mean))
   
-  p2 <- p + geom_ribbon(aes(x = Date, ymin = Min, ymax = Max), fill = "firebrick1", alpha = 0.5,
+  p2 <- p +
+    geom_rect(xmin = -Inf, xmax = as.numeric(as.Date("2012-09-18")),
+              ymin = -Inf, ymax = Inf, 
+              fill = "grey80", col = "grey80") +
+    geom_ribbon(aes(x = Date, ymin = Min, ymax = Max), fill = "firebrick1", alpha = 0.5,
                         data = DayTemp) +
+    geom_line(aes(x = Date, y = Mean), size = .5, col = "firebrick1", data = DayTemp) +
     geom_line(aes(x = Date, y = moist, col = co2), data = co.means) +
     scale_color_manual(values = c("blue", "red"), expression(CO[2]~treat), 
                        labels = c("Ambient", expression(eCO[2]))) +
@@ -45,7 +63,12 @@ FgMstTmpRn <- function(startDate = NULL, endDate = NULL){
     scale_x_date(breaks= date_breaks("2 month"),
                  labels = date_format("%b-%y"),
                  limits = c(startDate, endDate)) +
-    theme(axis.text.x  = element_text(angle=45, vjust= 1, hjust = 1)) +
-    geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed")
-  ggsavePP(filename= "output//Figs/FACE_TempMoistRain", plot = p2, width = 6, height = 6)
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          axis.text.x = element_text(angle=45, vjust= 1, hjust = 1, 
+                                     size = 13),
+          legend.position = "non",
+          axis.title.y = element_text(size = 15),
+          plot.title = element_text(size = 25, face = "bold"))
+  ggsavePP(filename= "output//Figs/GSBI_Poster/FACE_TempMoistRain", plot = p2, width = 6, height = 5)
 }
