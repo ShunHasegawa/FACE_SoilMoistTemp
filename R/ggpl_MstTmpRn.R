6a9342588cada34651be12b7428d3328fa8158af
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-03-13 13:30:00 +0000

    fig title, line col exc are readjusted

--- R/ggpl_MstTmpRn.R
+++ R/ggpl_MstTmpRn.R
@@ -13,13 +13,13 @@ FgMstTmpRn <- function(startDate = NULL, endDate = NULL){
   DayTemp <- ddply(tempDF, .(Date), summarise, Mean = mean(Mean, na.rm = TRUE), 
                    Min = mean(Min, na.rm = TRUE), 
                    Max = mean(Max, na.rm = TRUE))
-  DayTemp$variable <- "atop(Soil~temperature, at~10~cm~(degree~C))"
+  DayTemp$variable <- "Soil~temperature~(degree~C)"
   
   # daily mean for each co2 treatments for fig for publicaiton
   co2Temp <- ddply(tempDF, .(Date, co2), summarise, Mean = mean(Mean, na.rm = TRUE),
                    Min = mean(Min, na.rm = TRUE), 
                    Max = mean(Max, na.rm = TRUE))
-  co2Temp$variable <- "atop(Soil~temperature, at~10~cm~(degree~C))"
+  co2Temp$variable <- "Soil~temperature~(degree~C)"
   
   ############
   # Rainfall #
@@ -63,15 +63,19 @@ FgMstTmpRn <- function(startDate = NULL, endDate = NULL){
   science_theme <- theme(panel.grid.major = element_blank(),
                          panel.grid.minor = element_blank(),
                          axis.text.x  = element_text(angle=45, vjust= 1, hjust = 1),
-                         legend.position = c(.1, .93), 
-                         legend.title = element_blank())
+                         legend.position = c(.11, .93), 
+                         legend.title = element_blank(),
+                         legend.key = element_blank(),
+                         legend.key.width = unit(2, "lines"))
   
   p3 <- p + 
-    geom_line(aes(x = Date, y = Mean, linetype = co2, col = co2), data = co2Temp) +
-    geom_line(aes(x = Date, y = moist, linetype = co2, col = co2), data = co.means) +
-    scale_linetype_manual(values = c("solid", "dashed"), 
+    geom_line(aes(x = Date, y = Mean, linetype = co2, col = co2), 
+              data = co2Temp, alpha = .6, size = .7) +
+    geom_line(aes(x = Date, y = moist, linetype = co2, col = co2), 
+              data = co.means, alpha = .6, size = .7) +
+    scale_linetype_manual(values = c("solid", "twodash"), 
                           labels = c("Ambient", expression(eCO[2]))) +
-    scale_color_manual(values = c("grey80", "black"), 
+    scale_color_manual(values = c("grey40", "black"), 
                        labels = c("Ambient", expression(eCO[2]))) +
     geom_bar(aes(x = Date, y = Rain_mm_Tot), stat = "identity", data = allrain) +
     facet_grid(variable~. , scale = "free", labeller = label_parsed) +
